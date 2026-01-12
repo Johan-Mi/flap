a5fe0c35494f6f8e02f8af7a3c50b4d27d585eff
Johan-Mi <johanmi@protonmail.com>
2026-01-12 12:30:43 +0100

    ast: Store node ends instead of sizes

--- src/ast.rs
+++ src/ast.rs
@@ -1,6 +1,6 @@
 pub struct Tree {
     ops: Vec<crate::Op>,
-    sizes: Vec<usize>,
+    ends: Vec<usize>,
 }
 
 impl Tree {
@@ -24,8 +24,8 @@ impl Node<'_> {
     pub fn children(self) -> impl Iterator<Item = Self> {
         let tree = self.tree;
         let mut next = self.index + 1;
-        let end = self.index + tree.sizes[self.index];
-        core::iter::from_fn(move || (next != end).then(|| (next, next += tree.sizes[next]).0))
+        let end = tree.ends[self.index];
+        core::iter::from_fn(move || (next != end).then(|| (next, next = tree.ends[next]).0))
             .map(|index| Self { index, tree })
     }
 }
@@ -40,7 +40,7 @@ impl Default for Builder {
         Self {
             tree: Tree {
                 ops: Vec::new(),
-                sizes: Vec::new(),
+                ends: Vec::new(),
             },
             stack: Vec::new(),
         }
@@ -51,12 +51,11 @@ impl Builder {
     pub fn start_node(&mut self, op: crate::Op) {
         self.stack.push(self.tree.ops.len());
         self.tree.ops.push(op);
-        self.tree.sizes.push(0);
+        self.tree.ends.push(0);
     }
 
     pub fn finish_node(&mut self) {
-        let entry = self.stack.pop().unwrap();
-        self.tree.sizes[entry] = self.tree.ops.len() - entry;
+        self.tree.ends[self.stack.pop().unwrap()] = self.tree.ops.len();
     }
 
     pub fn parent(&self) -> crate::Op {
